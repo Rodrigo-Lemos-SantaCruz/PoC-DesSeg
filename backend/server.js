78f051e391ffc0d67b033bb489c37eb353d12f74
Rodrigo-Lemos-SantaCruz <rodrigo.lemos@unisantacruz.edu.br>
2025-03-31 07:54:19 -0300

    bcrypt hash

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const bcrypt = require('bcrypt')
 const app = express();
 const PORT = 3000;
 app.use(express.json());
@@ -12,14 +12,35 @@ const usuarios = {
     'user': { senha: 'abcd', funcao: 'user' }
 };
 
+// Simulação de BD com senhas criptografadas
+const usuariosSeguros = {
+    'admin': { senha: '$2a$12$NuTArywhAB5hlzdnnhmGgOwMWkUl/mfoN/6EGkU7k22ljOmald7EC', funcao: 'admin' }, // senha '1234'
+    'user': { senha: '$2a$12$eyG5Csoyt/5tVXkMoADt3OB.PxsO2qr6p7H9lx6w4CfReHl3S1THm', funcao: 'user' } // senha 'abcd'
+}
+
 // Rota de login
 app.post('/login', (req, res) => {
     const { nome, senha } = req.body;
-
-    if (usuarios[nome] && usuarios[nome].senha === senha) {
-        res.json({ sucesso: true, funcao: usuarios[nome].funcao });
-    } else {
-        res.status(401).json({ sucesso: false, mensagem: 'Usuário ou senha inválidos!' });
+    const usuario = usuariosSeguros[nome]
+    if(usuario) {
+        bcrypt.compare(senha, usuario.senha)
+            .then(resultado => {
+                if (resultado) {
+                    console.log(nome+' fez login com a senha '+senha)
+                    res.json({ sucesso: true, funcao: usuario.funcao })
+                }
+                else {
+                    console.error(nome+' tentou login com a senha '+senha)
+                    res.status(401).json({ sucesso: false, mensagem: 'Usuário ou senha inválidos!' })
+                }
+            })
+            .catch(erro => {
+                console.log(erro)
+                res.status(500).json({ sucesso: false, mensagem: erro })
+            })
+    }
+    else {
+        res.status(401).json({ sucesso: false, mensagem: 'Usuário ou senha inválidos!' })
     }
 });
 
